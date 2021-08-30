e7c3e1435e6e4f5089ebd6b89da56190039cb61d
dilciabarrios <dilciabarrios@gmail.com>
2021-08-30 07:12:42 -0500

    Referencia archivos .py, módulos: presupuesto y género

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# _*_ coding:utf-8 _*_
+
+{
+    'name': 'Modulo de peliculas',
+    'version': '1.0',
+    'depends': [
+        'contacts',
+    ],
+    'author': 'Dilcia Barrios',
+    'category': 'Peliculas',
+    'website': 'http://www.google.com',
+    'summary': 'Modulo de Presupuestos de peliculas',
+    'description': '''
+    Modulo para hacer presupuestos de peliculas
+    ''',
+    'data': [
+        'views/menu.xml',
+    ],
+}
